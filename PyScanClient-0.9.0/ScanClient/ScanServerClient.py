5f81ffad058faaeb67c3cce91fa4c91245a89b77
Dylan Maxwell <maxwelld@frib.msu.edu>
2015-03-02 11:31:51 -0500

    Add scan client methods get_scan() and get_data(), and implement ScanInfo and ScanData classes.

--- PyScanClient-0.9.0/ScanClient/ScanServerClient.py
+++ PyScanClient-0.9.0/ScanClient/ScanServerClient.py
@@ -5,7 +5,16 @@ Created on 30 12, 2014
 @author: Yongxiang Qiu
 '''
 
-import requests
+from datetime import datetime
+
+from collections import OrderedDict
+
+try:
+    from xml.etree import cElementTree as ElementTree
+except ImportError:
+    from xml.etree import ElementTree
+
+import requests, numpy
 
 class ScanServerClient(object):
     '''
@@ -145,6 +154,37 @@ class ScanServerClient(object):
             raise Exception, 'Failed to remove completed scan.'
         return r.status_code
     
+
+    def get_scan(self, scanID):
+        '''
+        Get information for the scan with the given ID.
+
+        :param scanID: scan ID
+        :return: ScanInfo object
+        '''
+        try:
+            # Not sure what content type this is requesting, should be XML.
+            r = requests.get(self.__baseURL+self.__scanResource+'/'+str(scanID))
+        except:
+            raise Exception, 'Failed to get info from scan '+str(scanID)
+        return ScanInfo(r.text)
+
+
+    def get_data(self, scanID):
+        '''
+        Get data for the scan with the given ID.
+
+        :param scanID: scan ID
+        :return: ScanData object
+        '''
+        try:
+            # Not sure what content type this is requesting, should be XML.
+            r = requests.get(self.__baseURL+self.__scanResource+'/'+str(scanID)+'/data')
+        except:
+            raise Exception, 'Failed to get data from scan '+str(scanID)
+        return ScanData(r.text)
+
+
     #############Detailed Design Needed#############
     def getScanInfo(self,scanID = None,infoType = None):
         '''
@@ -333,3 +373,134 @@ else:
     print 'All scans aborted.'
     '''
 
+class ScanInfo(object):
+    '''
+    The ScanInfo class contains scan details as returned by the Scan Server
+    and is constructed from the the raw XML response similar to the following:
+
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <scan>
+        <id>15</id>
+        <name>example1</name>
+        <created>1424465207911</created>
+        <state>Idle</state>
+        <runtime>0</runtime>
+        <total_work_units>22</total_work_units>
+        <performed_work_units>0</performed_work_units>
+        <address>-1</address>
+        <command/>
+    </scan>
+
+
+    :param xml: string containing scan information in XML format
+    '''
+    
+    _ROOT_TAG = "scan"
+    _ID_TAG = "id"
+    _NAME_TAG = "name"
+    _STATE_TAG = "state"
+    _CREATED_TAG = "created"
+    _ADDRESS_TAG = "address"
+    _RUNTIME_TAG = "runtime"
+    _COMMAND_TAG = "command"
+    _TOTAL_WORK_TAG = "total_work_units"
+    _COMPLETED_WORK_TAG = "performed_work_units"
+
+    def __init__(self, xml):
+        root = ElementTree.fromstring(xml)
+        if root.tag != self._ROOT_TAG:
+            raise ValueError("ScanInfo: Expecting root tag '{}' not '{}'".format(self._ROOT_TAG, root.tag))
+        self.id = int(root.findtext(self._ID_TAG))
+        self.name = root.findtext(self._NAME_TAG)
+        self.created = datetime.fromtimestamp(float(root.findtext(self._CREATED_TAG))/1000) # milliseconds to seconds
+        self.state = root.findtext(self._STATE_TAG)
+        self.runtime = root.findtext(self._RUNTIME_TAG)
+        self.total_work = int(root.findtext(self._TOTAL_WORK_TAG))
+        self.completed_work = int(root.findtext(self._COMPLETED_WORK_TAG))
+        self.address = int(root.findtext(self._ADDRESS_TAG))
+        self.command = root.findtext(self._COMMAND_TAG)
+    
+    def is_idle(self):
+        return (self.state == "Idle")
+    
+    def is_running(self):
+        return (self.state == "Running")
+    
+    def is_finished(self):
+        return (self.state == "Finished")
+        
+    def is_aborted(self):
+        return (self.state == "Aborted")
+    
+    def progress(self):
+        return ((100.0 * self.completed_work) / self.total_work)
+    
+    def __str__(self):
+        return "ScanInfo{{ id={info.id}, name='{info.name}', state={info.state} created='{info.created}' }}".format(info=self)
+
+
+
+class ScanData(object):
+    '''
+    The ScanData class contains the data from a running or finished scan and
+    is constructed from the raw XML response similar to the following:
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <data>
+        <device>
+            <name>D_M:LS1_CA01:BPM_D1144:POSH_RD</name>
+            <samples>
+                <sample id="0">
+                    <time>1424466313887</time>
+                    <value>-0.0147678</value>
+                </sample>
+                <sample id="1">
+                    <time>1424466313889</time>
+                    <value>-0.0148757</value>
+                </sample>
+                <!-- more samples -->
+            </samples>
+        </device>
+        <!-- more devices --> 
+    </data>
+
+    The ScanData class has the following properties:
+
+    times - dictionary of timestamp values as an NDArray and keyed by device name
+    values - dictionary of scan data values as an NDArray and keyed by device name
+    devices - list of device names
+
+    :param xml: string containing scan data in XML format
+    '''
+
+    _ROOT_TAG = "data"
+    _NAME_TAG = "name"
+    _TIME_TAG = "time"
+    _VALUE_TAG = "value"
+    _DEVICE_TAG = "device"
+    _SAMPLE_TAG = "sample"
+    _SAMPLES_TAG = "samples"
+    _SAMPLE_ID_ATT = "id"
+
+    def __init__(self, xml):
+        root = ElementTree.fromstring(xml)
+        if root.tag != self._ROOT_TAG:
+            raise ValueError("ScanData: Expecting root tag '{}' not '{}'".format(self._ROOT_TAG, root.tag))
+        self.times = OrderedDict()
+        self.values = OrderedDict()
+        for device in root.iter(self._DEVICE_TAG):
+            data = []
+            name = device.findtext(self._NAME_TAG)
+            for sample in device.find(self._SAMPLES_TAG).iter(self._SAMPLE_TAG):
+                sid = int(sample.get(self._SAMPLE_ID_ATT))
+                # Consider converting timestamp to datetime object.
+                data.append((sid, float(sample.findtext(self._TIME_TAG)),
+                                    float(sample.findtext(self._VALUE_TAG))))
+            data.sort(key=lambda x: x[0])
+            times = numpy.empty(len(data))
+            values = numpy.empty(len(data))
+            for idx in xrange(len(data)):
+                times[idx] = data[idx][1]
+                values[idx] = data[idx][2]
+            self.times[name] = times
+            self.values[name] = values
+        self.devices = list(self.values.keys())
